b689124f61c2103157117e930ab8141a5a3ff659
Rizki Awal R <rizkiawal16@gmail.com>
2023-05-09 08:41:21 +0700

    tugas beres

---
+++ moduleBiodata.js
@@ -0,0 +1,9 @@
+var nama = "Rizki Awal Ramadhani";
+var tempatLahir = "Bandung";
+var tanggalLahir = "5 November 2002";
+var alamat = "Jl.Sekeloa";
+
+console.log("Nama: ", nama);
+console.log("Tempat Lahir: ", tempatLahir);
+console.log("Tanggal Lahir: ", tanggalLahir);
+console.log("Alamat: ", alamat);
